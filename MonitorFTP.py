da5fe2bb1e2d7eb36f317b52f8a65e9636be457a
SandeepHooda <sonu.hooda@gmail.com>
2019-08-14 21:11:52 +0530

    Monitor camera

--- MonitorFTP.py
+++ MonitorFTP.py
@@ -9,6 +9,9 @@ from email.mime.multipart import MIMEMultipart
 import subprocess
 import os, sys, stat
 import requests
+import urllib.request
+import base64
+import traceback
 
 username = 'foscamnotificationsandeep@gmail.com'
 password = '' #//This password is in post master, raspberry pi, D3D cameras
@@ -27,13 +30,15 @@ Your camera is not working id : """
 
 
 class CameraStatus(object):
-    def __init__(self, id, status, ftpID):
+    def __init__(self, id, status, ftpID, ipAddress):
         self.id = id
         self.status = status
         self.ftpID = ftpID;
+        self.ipAddress = ipAddress;
 allCameraStatus = []
 
-allCameraStatus.append(CameraStatus("5", False, "1"))
+allCameraStatus.append(CameraStatus("5", False, "1", "192.168.0.151"))
+allCameraStatus.append(CameraStatus("8", False, "2", "192.168.0.150"))
 
 for obj in allCameraStatus:
     print (obj.id, obj.status)
@@ -51,6 +56,33 @@ def sendEmail(subject, body) :
         msg.attach(MIMEText(body, 'plain'))
         server.sendmail(sender_email, receiver_email, msg.as_string())
 
+def captureImage(path, ip) :
+  files = [f for f in glob.glob(path + "**/*.jpg", recursive=True)]
+  if (len(files) > 0):
+     print(" there are images already ")
+     return #Already has images no need to check further with camera
+  else:
+     print(" I wil capture image from camera")
+  try:
+     out_file_path=path+ip+"_imageCapture.jpg"
+     download_url="http://"+ip+"/tmpfs/auto.jpg?dummy="+str(time.time())
+     req = urllib.request.Request(download_url)
+     username="admin"
+     password="ForgetNot85"
+     credentials = ('%s:%s' % (username, password))
+     encoded_credentials = base64.b64encode(credentials.encode('ascii'))
+     req.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
+  
+     with urllib.request.urlopen(req) as response, open(out_file_path, 'wb') as out_file:
+       data = response.read()
+       out_file.write(data)
+     #print("check you image now:"+out_file_path) 
+     os.chmod( out_file_path, 0o777)
+     #time.sleep(60)  
+  except:
+     #traceback.print_exc();
+     print("could not connect to "+ip)
+
 def checkFtpLocation(path) :
     
     files = [f for f in glob.glob(path + "**/*.jpg", recursive=True)]
@@ -63,15 +95,17 @@ def checkFtpLocation(path) :
         return False
 
 while(True):
-    for obj in allCameraStatus:
-        try:
+    try:
+        for obj in allCameraStatus:
             print("Monitor on for "+obj.id)
-            cameraStatus = checkFtpLocation("/home/pi/ftp/d3d_"+obj.ftpID+"/")
+            path = "/home/pi/ftp/d3d_"+obj.ftpID+"/";
+            captureImage(path, obj.ipAddress)
+            cameraStatus = checkFtpLocation(path)
             if (cameraStatus != obj.status):
                 if (cameraStatus):
-                    sendEmail("camera is up","camera 1");
+                    sendEmail("camera is up","camera "+obj.ftpID);
                 else:
-                    sendEmail("camera is down","camera 1");
+                    sendEmail("camera is down","camera "+obj.ftpID);
                     
                 obj.status = cameraStatus
             # Send heart beat every 60 second FTP sends photo every 18 seconds
@@ -80,7 +114,8 @@ while(True):
                 requests.get(url+"N")
             else:
                 requests.get(url+"Y")
-        except:
-            print("Error in monitor")
+        time.sleep(60)
+    except:
+        print("Error in monitor")
         
-    time.sleep(60)
+    
